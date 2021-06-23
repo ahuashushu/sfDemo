f9ea0ee73e13d0c8a3ddafcf58140e73052c03f4
ahuashushu <1085535230@qq.com>
2021-06-23 12:03:04 +0800

    希尔、归并

--- src/main/java/com/example/demo/May/Sorting.java
+++ src/main/java/com/example/demo/May/Sorting.java
@@ -85,12 +85,89 @@ public class Sorting {
         return nums;
     }
 
+    /**
+     * 希尔排序
+     * 先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，具体算法描述：
+     * 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+     * 按增量序列个数k，对序列进行k 趟排序；
+     * 每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+     *
+     * 时间复杂度：O(n^1.3)
+     * 空间复杂度：O(1)
+     * @param nums
+     * @return
+     */
+    public static int[] shellSort(int[] nums){
+        for (int temp = nums.length / 2; temp > 0; temp /= 2){
+            for (int i = temp; i < nums.length; i++){
+                int j = i;
+                int current = nums[j];
+                while (j - temp >= 0 && nums[j - temp] > current){
+                    nums[j] = nums[j - temp];
+                    nums[j - temp] = current;
+                }
+            }
+        }
+        return nums;
+    }
+
+    /**
+     * 归并算法
+     * 把长度为n的输入序列分成两个长度为n/2的子序列；
+     * 对这两个子序列分别采用归并排序；
+     * 将两个排序好的子序列合并成一个最终的排序序列。
+     *
+     * 时间复杂度：O(nlog2n)
+     * 空间复杂度：O(n)
+     * @param nums
+     */
+    public static void mergeSort(int[] nums){
+        mergeSortRange(nums,0,nums.length);
+    }
+
+    private static void mergeSortRange(int[] nums, int left, int right){
+        if (right - left <= 1){
+            return;
+        }
+        int mid = (left + right) / 2;
+        mergeSortRange(nums, left , mid);
+        mergeSortRange(nums, mid, right);
+        merge(nums,left,mid,right);
+    }
+
+    private static void merge(int[] nums, int left, int mid, int right){
+        int[] arry = new int[right - left];
+        int leftHead = left;
+        int rightHead = mid;
+        int i = 0;
+        while (leftHead < mid && rightHead < right){
+            if (nums[leftHead] < nums[rightHead]){
+                arry[i++] = nums[leftHead++];
+            }else {
+                arry[i++] = nums[rightHead++];
+            }
+        }
+        while (leftHead < mid){
+            arry[i++] = nums[leftHead++];
+        }
+        while (rightHead < right){
+            arry[i++] = nums[rightHead++];
+        }
+        for (int j = 0; j < arry.length; j++){
+            nums[left + j] = arry[j];
+        }
+    }
+
     public static void main(String[] args) {
         //冒泡
-        bubbleSort(new int[]{5,3,15,-9,0,3,1,62,24});
-        //选择
-        selectionSort(new int[]{5,3,15,-9,0,3,1,62,24});
-        //插入
-        insertionSort(new int[]{5,3,15,-9,0,3,1,62,24});
+//        bubbleSort(new int[]{5,3,15,-9,0,3,1,62,24});
+//        //选择
+//        selectionSort(new int[]{5,3,15,-9,0,3,1,62,24});
+//        //插入
+//        insertionSort(new int[]{5,3,15,-9,0,3,1,62,24});
+        //希尔
+//        shellSort(new int[]{5,3,15,-9,0,3,1,62,24});
+        //归并
+        mergeSort(new int[]{5,3,15,-9,0,3,1,62,24});
     }
 }
